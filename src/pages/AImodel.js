40080d16d52b89fc4fe6b54ef34d402015f6c10a
ABHISHEKBEDGE <abhishekbedge1947@gmail.com>
2024-02-14 11:12:35 +0530

    Initial commit

---
+++ src/pages/AImodel.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AImodel = () => {
+    return <div className='dark:text-white'>AI model</div>
+}
+
+export default AImodel
---
+++ src/pages/PatientRecords.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PatientRecords= () => {
+    return <div className='dark:text-white'>Patient Records</div>
+};
+
+export default PatientRecords;
